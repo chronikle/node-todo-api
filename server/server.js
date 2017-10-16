2711c743224ea69131190b6b05ef018f1959d48f
Chronikle <alexandru.caprarescu@plaut.ro>
2017-10-16 16:36:11 +0300

    Setup app for Heroku NO DB

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
@@ -48,6 +49,6 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(8080, () => {
-    console.log('Started on port 8080');
+app.listen(PORT, () => {
+    console.log(`Started on port ${port}`);
 });
